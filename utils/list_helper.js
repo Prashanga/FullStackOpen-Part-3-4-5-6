83d8a926f798dba0f6ef638ff3a534c44b42ed9d
prashanga <prashanga@gmail.com>
2020-02-24 21:12:36 +1100

    Continued with tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const likesTotal = blogs.reduce((sum,blog) => {
+    return sum + blog.likes
+  },0)
+  console.log(likesTotal)
+  return likesTotal
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
