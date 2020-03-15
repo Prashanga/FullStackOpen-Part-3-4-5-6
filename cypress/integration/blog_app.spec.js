f3296daef282d0f89f9af3a8492cc2dbd07514aa
prashanga <prashanga@gmail.com>
2020-03-15 20:07:26 +1100

    5.17 rectified

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,6 +1,14 @@
 describe('Blog app', function() {
     beforeEach(function() {
-      cy.request('POST', 'http://localhost:3003/api/testing/reset')
+
+     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+
+      const user = {
+          name: 'Test From Cypress',
+          username: 'test',
+          password: 'test'
+        }
+      cy.request('POST', 'http://localhost:3003/api/users/', user)
       cy.visit('http://localhost:3000')
     })
 
